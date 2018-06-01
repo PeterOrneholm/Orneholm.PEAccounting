f635fb54ca39bc7dcd0b4d6f57ebb56edbdefc6d
Peter Örneholm <peter@orneholm.com>
2018-06-01 14:01:46 +0200

    Add constants

---
+++ src/Orneholm.PEAccountingNet/Models/ClientDeliveryTypeConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Orneholm.PEAccountingNet.Models
+{
+    public static class ClientDeliveryTypeConstants
+    {
+        public const string Mail = "Mail";
+        public const string Email = "Email";
+        public const string Manually = "Manually";
+        public const string Efaktura = "Efaktura";
+    }
+}
---
+++ src/Orneholm.PEAccountingNet/Models/CurrencyConstants.cs
@@ -0,0 +1,39 @@
+﻿namespace Orneholm.PEAccountingNet.Models
+{
+    /// <summary>
+    /// Some common currencies
+    /// </summary>
+    public static class CurrencyConstants
+    {
+        /// <summary>
+        /// Euro
+        /// </summary>
+        public const string Eur = "EUR";
+
+        /// <summary>
+        /// US Dollar
+        /// </summary>
+        public const string Usd = "USD";
+
+        /// <summary>
+        /// British pound
+        /// </summary>
+        public const string Gbp = "GBP";
+
+
+        /// <summary>
+        /// Sweden Krona
+        /// </summary>
+        public const string Sek = "SEK";
+
+        /// <summary>
+        /// Norway Krone
+        /// </summary>
+        public const string Nok = "NOK";
+
+        /// <summary>
+        /// Denmark Krone
+        /// </summary>
+        public const string Dkk = "DKK";
+    }
+}
